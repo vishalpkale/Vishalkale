6997c4e8efdacc017d2e6c9c1b070cb771ce7c35
Bhumi <nagbhumi02@gmail.com>
2022-04-27 21:01:47 +0530

    deletefiles

--- src/Controllers/blogsController.js
+++ src/Controllers/blogsController.js
@@ -1,63 +1,110 @@
-const mongoose=require('mongoose')
-const authorModel=require('../Models/authorModel')
-const blogsModel=require('../Models/blogsModel')
+const mongoose = require('mongoose')
+const authorModel = require('../Models/authorModel')
+const blogsModel = require('../Models/blogsModel')
 
 const createBlog = async function (req, res) {
-    try {
-      const requestBody = req.body;
-      if (!requestBody) {
-        return res.status(400).send({
-          status: false,
-          message: "Invalid request parameters. Please provide blog details",
-        });
-      }
-  
-      //Extract params
-      const { title, body, authorId, tags, category} =
-        requestBody;
-  
-      // Validation starts
-      if (!title) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Blog Title is required" });
-      }
-      if (!body) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Blog body is required" });
-      }
-      if (!authorId) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Author id is required" });
-      }
-      if (!tags) {
-        return res.status(400).send({
-          status: false,
-          message: " tags are not valid",
-        });
-      }
-      if (!category) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Blog category is required" });
-      }
-      const findAuthor = await authorModel.findById(authorId);
-      if (!findAuthor) {
-        return res
-          .status(400)
-          .send({ status: false, message: `Author does not exists.` });
-      }
-      const createdata = await blogsModel.create(requestBody)
-      console.log(createdata)
-      res.status(201).send({status: true, data: createdata})
+  try {
+    const requestBody = req.body;
+    if (!requestBody) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. Please provide blog details",
+      });
     }
-    catch(error) {
-        console.log(error)
-        res.status(500).send({ msg: error.message })
+
+    //Extract params
+    const { title, body, authorId, tags, category } =
+      requestBody;
+
+    // Validation starts
+    if (!title) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Blog Title is required" });
     }
+    if (!body) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Blog body is required" });
+    }
+    if (!authorId) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Author id is required" });
+    }
+    if (!tags) {
+      return res.status(400).send({
+        status: false,
+        message: " tags are not valid",
+      });
+    }
+    if (!category) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Blog category is required" });
+    }
+    const findAuthor = await authorModel.findById(authorId);
+    if (!findAuthor) {
+      return res
+        .status(400)
+        .send({ status: false, message: `Author does not exists.` });
+    }
+    const createdata = await blogsModel.create(requestBody)
+    console.log(createdata)
+    res.status(201).send({ status: true, data: createdata })
+  }
+  catch (error) {
+    console.log(error)
+    res.status(500).send({ msg: error.message })
+  }
 }
-    
+
+//////////////////// Delete Api ///////////////////////////////////////////
+const deleteblog = async function (req, res) {
+
+  try {
+    let BlogId = req.params.BlogId;
+    let Blog = await blogsModel.findById(BlogId);
+    if (!Blog) {
+      return res.status(404).send({ status: false, msg: "Does not exists" });
+    }
+
+    let deletedblog = await blogsModel.findOneAndUpdate(
+      { _id: BlogId },
+      { $set: { isDeleted: true } },
+      { new: true },
+    );
+
+    res.status(200).send({ status: true, data: deletedblog });
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  };
+}
+
+let deletedByQueryParams = async function (req, res) {
+  try {
+    let data = req.query;
+
+    if (data) {
+      let deletedBlogsFinal = await blogsModel.updateMany(
+        { $in: data },
+        { $set: { isDeleted: true }, deletedAt: Date.now() },
+        { new: true }
+      );
+
+      res.status(200).send({ status: true, result: deletedBlogsFinal });
+    } else {
+      res.status(400).send({ ERROR: "BAD REQUEST" });
+    }
+  } catch (err) {
+    res.status(500).send({ ERROR: err.message });
+  }
+};
+
+
 module.exports.createBlog = createBlog
+module.exports.deleteblog = deleteblog
+module.exports.deletedByQueryParams = deletedByQueryParams
 
--- src/route/route.js
+++ src/route/route.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const express= require("express")
-=======
-const express = require('express');
->>>>>>> 878ce5dc9c06620a6fb010f544e2411bf088761c
 const router = express.Router();
 const authorController = require("../Controllers/authorController")
 const blogsController = require("../Controllers/blogsController")
@@ -13,8 +9,8 @@ router.post("/Author",authorController.createAuthor )
 //createBlogs
 router.post("/Blogs",blogsController.createBlog)
 
+router.delete("/Blogs/:BlogId",blogsController.deleteblog)
 
-
-
+router.delete("/blogs?queryparams",blogsController.deleteblog)
 
 module.exports = router;
