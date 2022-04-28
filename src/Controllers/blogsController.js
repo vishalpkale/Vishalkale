c763526eba1cb350379202a5134efc0f385f99de
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-04-28 12:47:00 +0530

    finaaaal

--- src/Controllers/blogsController.js
+++ src/Controllers/blogsController.js
@@ -165,10 +165,11 @@ const deleteblog = async function (req, res) {
     if (!Blog) {
       return res.status(404).send({ status: false, msg: "Does not exists" });
     }
-    if(chkid.isDeleted==true)
+    
+    if(Blog.isDeleted==true)
     {
       res.status(404).send({status:false,msg:"The document is deleted"})
-
+    }
     let deletedblog = await blogsModel.findOneAndUpdate(
       { _id: BlogId },
       { $set: { isDeleted: true } },
