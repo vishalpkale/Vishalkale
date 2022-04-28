8616f38e0f37db863ec2401c9d295bd541be5494
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-04-28 16:10:21 +0530

    commite

---
+++ src/middleware/commonMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const tokenValidation = function(req, res, next) {
+
+    try {
+        let jwttoken = req.headers["X-AUTH-TOKEN"];
+
+    if (!jwttoken) {
+        jwttoken = req.headers["x-auth-token"];
+    }
+
+    if (!jwttoken) {
+        jwttoken = req.headers["X-Auth-Token"];
+        }
+
+    if (!jwttoken) {
+    return res.status(400).send({ status: false, msg: "Token must be present in headers" });
+    }
+    console.log(jwttoken);
+   
+    let verifyAuthor = jwt.verify(jwttoken, "FunctionUp-Uranium");
+
+  if (!verifyAuthor){
+    return res.send({ status: false, msg: "Token is incorrect" });
+  }
+  next();
+
+}
+   catch (error) {
+    return res.status(500).send({ msg: error.message })
+   }
+}
